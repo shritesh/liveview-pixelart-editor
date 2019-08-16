2e09108e8a6b5c227ee7cb3a770c67d66284d6f3
Shritesh Bhattarai <shritesh@shritesh.com>
2019-08-16 11:43:20 -0500

    Switch to hex color values

--- lib/pixel_art_web/live/canvas_live.ex
+++ lib/pixel_art_web/live/canvas_live.ex
@@ -14,15 +14,24 @@ defmodule PixelArtWeb.CanvasLive do
   def mount(%{path_params: %{"topic" => topic}}, socket) do
     {:ok, canvas_pid} = get_canvas_pid(topic)
     canvas = Agent.get(canvas_pid, &Canvas.list/1)
-    {:ok, assign(socket, %{topic: topic, canvas_pid: canvas_pid, canvas: canvas})}
+
+    {:ok,
+     assign(socket, %{
+       topic: topic,
+       canvas_pid: canvas_pid,
+       canvas: canvas,
+       current_color: "#000000"
+     })}
   end
 
   def render(assigns) do
     ~L"""
-    <svg width="1000" height="750" style="--current-color:red;">
+    <form phx-change="change">
+      <input name="color" type="color" value="<%= @current_color %>" />
+    </form>
+    <svg width="1000" height="750" style="--current-color:<%= @current_color %>;">
     <%= for x <- 0..99, y <- 0..74 do %>
-      <% {r,g,b} = Canvas.get(@canvas,{x,y}) %>
-      <rect class="pixel" phx-click="click" phx-value="<%= "#{x},#{y}" %>" width="10" height="10" x="<%= x * 10 %>" y="<%= y * 10 %>" style="--pixel-color:rgb(<%= "#{r},#{g},#{b}" %>)" />
+      <rect class="pixel" phx-click="click" phx-value="<%= "#{x},#{y}" %>" width="10" height="10" x="<%= x * 10 %>" y="<%= y * 10 %>" style="--pixel-color:<%= Canvas.get(@canvas,{x,y}) %>;" />
     <% end %>
     </svg>
     """
@@ -31,8 +40,12 @@ defmodule PixelArtWeb.CanvasLive do
   def handle_event("click", coordinates, socket) do
     [x, y] = coordinates |> String.split(",") |> Enum.map(&String.to_integer/1)
 
-    Agent.update(socket.assigns.canvas_pid, &Canvas.put(&1, {x, y}, {100, 0, 0}))
+    Agent.update(socket.assigns.canvas_pid, &Canvas.put(&1, {x, y}, socket.assigns.current_color))
     canvas = Agent.get(socket.assigns.canvas_pid, &Canvas.list/1)
     {:noreply, assign(socket, :canvas, canvas)}
+  end
+
+  def handle_event("change", %{"color" => color}, socket) do
+    {:noreply, assign(socket, :current_color, color)}
   end
 end
--- test/pixel_art/canvas_test.exs
+++ test/pixel_art/canvas_test.exs
@@ -7,7 +7,7 @@ defmodule PixelArt.CanvasTest do
   end
 
   test "get/2 returns white by default", %{canvas: canvas} do
-    assert Canvas.get(canvas, {10, 10}) == {255, 255, 255}
+    assert Canvas.get(canvas, {10, 10}) == "#ffffff"
   end
 
   test "get/2 errors for OOB", %{canvas: canvas} do
@@ -16,22 +16,18 @@ defmodule PixelArt.CanvasTest do
 
   test "put/3 updates the color", %{canvas: canvas} do
     coordinate = {10, 10}
-    color = {1, 2, 3}
+    color = "#abc"
     canvas = Canvas.put(canvas, coordinate, color)
 
     assert Canvas.get(canvas, coordinate) == color
   end
 
   test "put/3 errors for OOB", %{canvas: canvas} do
-    catch_error(Canvas.put(canvas, {-1, -1}, {10, 10, 10}))
-  end
-
-  test "put/3 errors for OOB color", %{canvas: canvas} do
-    catch_error(Canvas.put(canvas, {10, 10}, {258, 10, 10}))
+    catch_error(Canvas.put(canvas, {-1, -1}, "red"))
   end
 
   test "list/1 returns sparse map of colors", %{canvas: canvas} do
-    data = %{{10, 10} => {1, 2, 3}, {20, 20} => {0, 0, 0}}
+    data = %{{10, 10} => "yellow", {20, 20} => "green"}
 
     canvas =
       Enum.reduce(data, canvas, fn {coordinate, color}, canvas ->
